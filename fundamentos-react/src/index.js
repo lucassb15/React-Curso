eee9de36378c7cf4441433b7032ce824ef3190bc
Lucas Soares <soares_lucas13@hotmail.com>
2022-03-14 23:29:07 -0300

    react-course

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
+    document.getElementById('root')
+)
